d37812392cac20d30a69f68ffc6a99986a6ce339
Naoki Ishimura <mirontest1460@gmail.com>
2017-11-29 19:58:25 +0900

    Refactoring

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -6,56 +6,66 @@ class MapController < ApplicationController
 
     # 円
     @radius = 100
-    # マーカー
 
-    @location = []
-    @location.push({
-      label: 'in',
-      lat: 35.681298-0.005,
-      lng: 139.766247,
-      icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
-      })
-    @location.push({
-      label: 'in',
-      lat: 35.681298+0.005,
-      lng: 139.766247,
-      icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
-      })
-    @location.push({
-      label: 'in',
-      lat: 35.681298,
-      lng: 139.766247-0.005,
-      icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
-      })
-    @location.push({
-      label: 'in',
-      lat: 35.681298,
-      lng: 139.766247+0.005,
-      icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
-      })
+    # マーカー
+    @location = set_location
 
     # 情報ウィンドウ
-    @window = []
-    @window.push({
-      label: 'out',
-      lat: 35.681298-0.0025,
-      lng: 139.766247,
-      })
-    @window.push({
-      label: 'out',
-      lat: 35.681298+0.0025,
-      lng: 139.766247,
-      })
-    @window.push({
-      label: 'out',
-      lat: 35.681298,
-      lng: 139.766247-0.0025,
-      })
-    @window.push({
-      label: 'out',
-      lat: 35.681298,
-      lng: 139.766247+0.0025,
-      })
+    @window = set_window
 
+  end
+
+  private
+
+  def set_location
+    location = []
+    location.push({
+                       label: 'in',
+                       lat: 35.681298-0.005,
+                       lng: 139.766247,
+                       icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
+                   })
+    location.push({
+                       label: 'in',
+                       lat: 35.681298+0.005,
+                       lng: 139.766247,
+                       icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
+                   })
+    location.push({
+                       label: 'in',
+                       lat: 35.681298,
+                       lng: 139.766247-0.005,
+                       icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
+                   })
+    location.push({
+                       label: 'in',
+                       lat: 35.681298,
+                       lng: 139.766247+0.005,
+                       icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1',
+                   })
+  end
+
+  def set_window
+    window = []
+    window.push({
+                    label: 'out',
+                    lat: 35.681298-0.0025,
+                    lng: 139.766247,
+                })
+    window.push({
+                    label: 'out',
+                    lat: 35.681298+0.0025,
+                    lng: 139.766247,
+                })
+    window.push({
+                    label: 'out',
+                    lat: 35.681298,
+                    lng: 139.766247-0.0025,
+                })
+    window.push({
+                    label: 'out',
+                    lat: 35.681298,
+                    lng: 139.766247+0.0025,
+                })
   end
 end
